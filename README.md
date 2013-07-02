640f41dca6fd424eab9c68b9aad99faf506c7901
cyle <cyle@standingdesk3>
2013-07-02 11:01:10 -0400

    - first commit!

---
+++ README.md
@@ -0,0 +1,42 @@
+# CyleSoft Git Update All
+
+This Ruby script simply runs `git remote update` and `git status -sb` below a tree of directories where your git projects live. It'll then tell you which git repos are clean, which have pending changes, which are behind or ahead of remote branches, etc.
+
+Basically I wanted to re-create the awesome bookmarks list in the [SourceTree](http://www.sourcetreeapp.com/) app, but on a linux-based system. I haven't found a program to do this, so I built this script.
+
+## Requirements
+
+This was built with Ruby 1.9.3, that's all. I don't know if it'll work with Ruby 1.8 or 2.0, though I'm fairly certain it'll run with 2.0.
+
+## Usage
+
+If you call the script with no arguments, simply `./git-update-all.rb`, it'll use the current directory to scan for repos.
+
+You can pass a directory path to it as an argument and it'll scan that directory for git repos, i.e. `./git-update-all.rb /path/to/projects`
+
+See next section for an example.
+
+## Example Usage
+
+This script depends on you having a directory that's full of your projects. If one of those project folders is a git repo, this script will check on it.
+
+For example, say you have a directory called **projects** that has the following contents:
+
+    some-project/
+    some-other-project/
+    another-project/
+    some-non-git-project/
+
+and each one of those directories has a git repo in it, except for that last one. If you ran:
+
+    /git-update-all.rb /path/to/projects
+
+You'd get a list of each git repo, whether it's clean, whether it has untracked changes, push/pulling to be done, etc.
+
+Colorized, too. Very pretty. That's all.
+
+## Notes
+
+If git is not installed in `/usr/bin/git` you should edit the "git_program_path" variable in git-update-all.rb
+
+This uses a built-in class to colorize terminal output, so no gems are necessary. I lifted this from [here](http://stackoverflow.com/questions/1489183/colorized-ruby-output).
---
+++ git-update-all.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby
+#
+# cylesoft git-update-all script
+#
+# go through directories, find .git folders, run git commands,
+# check local git status, check if there's anything to pull from remotes
+
+# if the short status equals this, it means the repo is clean and no action is required
+repo_is_clean_check = "## master\n"
+
+# set this to where git is installed
+git_program_path = "/usr/bin/git"
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+  
+  def lightred
+	colorize("1;31")
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+end
+
+use_path = "./"
+
+if ARGF.argv.length > 0 and File.exists?(ARGF.argv[0]) and File.directory?(ARGF.argv[0])
+	use_path = ARGF.argv[0];
+	puts "Updating (local and remote) and showing statuses of repositories below #{use_path.yellow}"
+else
+	puts "Updating (local and remote) and showing statuses of repositories below " + "this directory".yellow
+end
+
+puts ""
+
+use_path = File.expand_path(use_path) + "/"
+
+Dir.foreach(use_path) do |entry|
+	full_path = use_path + entry
+	#puts "Found: #{entry}"
+	#puts "File: #{entry}" if File.file?(full_path)
+	#puts "Dir: #{entry}" if File.directory?(full_path)
+	unless entry.eql?(".") or entry.eql?("..") or File.file?(full_path)
+		if File.exists?(full_path + "/.git/") and File.directory?(full_path + "/.git/")
+			#working_path = File.expand_path(entry);
+			working_path = full_path
+			git_path = working_path + "/.git/";
+			# update remote(s)
+			git_remote_update_result = `#{git_program_path} --git-dir="#{git_path}" --work-tree="#{working_path}" remote update`
+			# get and show status
+			git_status_result = `#{git_program_path} --git-dir="#{git_path}" --work-tree="#{working_path}" status -sb`
+			puts "status for #{entry}" if use_path.eql?("./")
+			puts "status for #{working_path}" unless use_path.eql?("./")
+			#puts git_status_result
+			puts "clean, no worries".green if git_status_result.eql?(repo_is_clean_check)
+			puts git_status_result.lightred unless git_status_result.eql?(repo_is_clean_check)
+			puts "" if git_status_result.eql?(repo_is_clean_check)
+		end
+	end
+end
